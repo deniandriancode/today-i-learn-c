89655ca0b602283b7b57f11f25f48f90d12eb4fb
Deni Andrian Prayoga <deniandriancode@gmail.com>
2023-02-14 13:34:11 +0700

    finished timer program v0.1.0

--- miniprograms/timer/src/main.c
+++ miniprograms/timer/src/main.c
@@ -1,8 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+void timer_run(char *dur);
+void timer_format(int idur);
+
+/* TODO: Use regex to detect error of user input */
+/* TODO: Add miniaudio to playback simple sound when finish */
+/* TODO: Use termios to change the attribute of terminal (keypress detection) */
+/* TODO: Add argument parser */
 int
 main(int argc, char **argv)
 {
-    printf("Hello world\n");
-    return 0;
+    /* get argument number */
+    switch (argc) {
+        case 1:
+            fprintf(stderr, "Please provide duration time\n");
+            return EXIT_FAILURE;
+        case 2:
+            timer_run(*(argv + 1));
+            break;
+        default:
+            fprintf(stderr, "Argument must exactly one: Expected 1 get %d\n", argc);
+            return EXIT_FAILURE;
+    }
+
+    /* count down to zero */
+
+    /* finish */
+    return EXIT_SUCCESS;
+}
+
+/* main timer function */
+void
+timer_run(char *dur)
+{
+    int idur;
+    
+    idur = (int) strtoul(dur, NULL, 10);
+    if (idur < 0) { /* negative duration, exit program */
+        fprintf(stderr, "Duration cannot less than zero\n");
+        exit(EXIT_FAILURE);
+    } else if (idur == 0) { /* either 0 or non number, exit program */
+        printf("Finish\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    timer_format(idur);
+}
+
+/* format the display */
+void
+timer_format(int idur)
+{
+    int h, m, s;
+    int count;
+
+    count = idur;
+
+    printf("\033[34m");
+    while (count > 0) {
+        h = count / 60 / 60;
+        m = count / 60;
+        s = count % 60;
+        printf("%d:%d:%d ", h, m, s);
+        printf("Remaining: %d second(s)\n", s);
+        --count;
+        sleep(1);
+        printf("\033[A\033[K");
+    }
+    printf("Finish\n");
+    printf("\033[0m");
 }
